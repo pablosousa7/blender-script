40f4aabb6ec4a488699f6927504f66013fee7749
Pablo Sousa <pablofroz@hotmail.com.br>
2022-05-28 01:54:27 -0300

    Create SimplesMoveObject.py
    Simple plugin to move between collections within blender
    
    	This first function moves all objects between collections, you have to pass 3 arguments
    	moveAllObjectsAnotherCollection(cFrom, cTo, stayObjs)
    
    	cFrom = collection from which objects will be extracted
    	cTo = collection where new objects will be stored
    	stayObjs = if you want to keep the objects in the original collection or remove them.
    
    
    	the second function does the same thing, but you can only move one file at a time.
    	
    	Author: Pablo Sousa

---
+++ SimplesMoveObject.py
@@ -0,0 +1,75 @@
+import bpy
+
+"""
+
+	Simple plugin to move between collections within blender
+
+	This first function moves all objects between collections, you have to pass 3 arguments
+	moveAllObjectsAnotherCollection(cFrom, cTo, stayObjs)
+
+	cFrom = collection from which objects will be extracted
+	cTo = collection where new objects will be stored
+	stayObjs = if you want to keep the objects in the original collection or remove them.
+
+
+	the second function does the same thing, but you can only move one file at a time.
+	
+	Author: Pablo Sousa
+"""
+
+def moveAllObjectsAnotherCollection(cFrom, cTo, stayObjs = False):
+    
+    # Get the collection "from"
+    coll_from = bpy.data.collections[cFrom]
+    
+    # Get the collection "to"
+    coll_to = bpy.data.collections[cTo]
+    
+    # clear all object 'from' collection
+    obj_unlink = []
+    
+    for obj in coll_from.objects:
+        # try link objects to the new collections
+        try:
+            coll_to.objects.link(obj)
+        except RuntimeError:
+            pass
+        
+        # Add all object to link unlink / delete
+        obj_unlink.append(obj)
+        
+    
+    # Here all objects inside list are deleted
+    if stayObjs == False:
+        for obj_del in obj_unlink:
+            coll_from.objects.unlink(obj_del)
+        
+
+def moveJustOneObject(collFrom, obj, collTo, stayObjt = False):
+    
+    # If collection is not empty then true or false
+    hasObj = len(list(bpy.data.collections[collFrom].all_objects)) > 0
+     
+    # Here get the object specific in the collection 'from'
+    if hasObj:
+        coll_from_obj = bpy.data.collections[collFrom].all_objects[obj]
+        
+        # add the object for delete
+        obj_unlink = coll_from_obj
+    else:
+        print("This object not found in this collection!")
+        
+    
+    if hasObj:
+        # add object the another collection
+        coll_to = bpy.data.collections[collTo].objects.link(coll_from_obj)
+    else:
+        print("This object not found in this collection!")
+        
+    # remove object of the current view
+    if stayObjt == False:
+        bpy.data.collections[collFrom].objects.unlink(obj_unlink)
+
+        
+moveAllObjectsAnotherCollection('DEMO.001', 'Collection', False)
+#moveJustOneObject('DEMO.001', 'Cube', 'Collection')
